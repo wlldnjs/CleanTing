445ec007da782fa161f896bec4f666b1dc07e55f
JINYOUNG\진영 <tadpolejjin95@gmail.com>
2017-07-06 00:54:46 +0900

    기본청소 보러가기, 총 금액 수정

---
+++ CleanTing/app/src/main/java/sopt/client/cleanting/MyRequest/MoreViewClean.java
@@ -0,0 +1,15 @@
+package sopt.client.cleanting.MyRequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sopt.client.cleanting.R;
+
+public class MoreViewClean extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_view_clean);
+    }
+}
