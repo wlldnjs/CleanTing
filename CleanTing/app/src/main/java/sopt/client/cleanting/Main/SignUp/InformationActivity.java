60bb756feb08552a04d8e281b7094e0239dbc947
JINYOUNG\진영 <tadpolejjin95@gmail.com>
2017-07-07 17:28:39 +0900

    xml

--- CleanTing/app/src/main/java/sopt/client/cleanting/Main/SignUp/InformationActivity.java
+++ CleanTing/app/src/main/java/sopt/client/cleanting/Main/SignUp/InformationActivity.java
@@ -1,9 +1,7 @@
 package sopt.client.cleanting.Main.SignUp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 import sopt.client.cleanting.R;
@@ -15,15 +13,7 @@ public class InformationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
 
-        button_back=(ImageView)findViewById(R.id.button_back);
 
-        button_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(),SignUpActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
     }
 }
